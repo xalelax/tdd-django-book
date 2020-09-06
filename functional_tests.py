95f377381422c5551b3b5d922fe5d957be50a7c0
Alessandro Angioi <alessandro.angioi@hotmail.com>
2020-09-06 17:39:30 +0200

    Restructure functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-browser.get('http://localhost:8000')
+    def tearDown(self):
+        self.browser.quit()
 
-assert 'Django' in browser.title
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Visit homepage
+        self.browser.get('http://localhost:8000')
+
+        # Check it has the correct title
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # TODO: insert rest of visitor journey
+
+if __name__ == '__main__':
+    unittest.main()
