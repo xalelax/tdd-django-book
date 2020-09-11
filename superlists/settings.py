3d59bd80b0fc99ba5c543c2a4ab25b1d37e36f1a
Alessandro Angioi <alessandro.angioi@hotmail.com>
2020-09-11 23:02:31 +0200

    Hack ALLOWED_HOSTS to be *

--- superlists/settings.py
+++ superlists/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '7st-)dwiceue+baf8gxpsmabkp86(@@_1tn5up$ej*x=bajpla'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
